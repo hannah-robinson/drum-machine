401222781659496ddce0ba12c67762bb9d95369a
Hannah Robinson <hannah.robinson.git@gmail.com>
2021-07-14 23:34:15 +1200

    Add js object containing all properties that will change with user interaction

---
+++ script.js
@@ -0,0 +1,51 @@
+$(function(){
+
+  const drumPadDetails = [
+    {
+      id: "Q",
+      border: "popsicle",
+      display: "Clap"
+    },
+    {
+      id: "W",
+      border: "sunshine",
+      display: "Open High Hat"
+    },
+    { id: "E",
+      border: "sea",
+      display: "Kick"
+    },
+    {
+      id: "A",
+      border: "smurf",
+      display: "Closed High Hat"
+    },
+    {
+      id: "S",
+      border: "leaf",
+      display: "Kick 'n' Hat"
+    },
+    {
+      id: "D",
+      border: "peach",
+      display: "Shaker"
+    },
+    {
+      id: "Z",
+      border: "lagoon",
+      display: "Punchy Kick"
+    },
+    {
+      id: "X",
+      border: "bubble-gum",
+      display: "Side Stick"
+    },
+    {
+      id: "C",
+      border: "violet-sky",
+      display: "Snare"
+    }
+  ]
+}); 
+
+
