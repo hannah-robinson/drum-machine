570c240586d28c172f8be8550e48e451a704f444
Hannah Robinson <hannah.robinson.git@gmail.com>
2021-07-16 17:06:10 +1200

    Refactor js

--- main.js
+++ main.js
@@ -3,18 +3,25 @@ import drumPadDetails from './modules/drumPadDetails.js'
 $(function(){
  
   const drumBeatHandler = (e) => {
-    // Get the id for a given drum pad when the user selects it either by clicking on it or pressing its keyboard shortcut
+    const audioID = getAudioID(e);
+    // Use the object id to select the chosen drum pad object
+    const drumPad = drumPadDetails.find(pad => pad.id === audioID);
+    updateUI(drumPad);
+  }
+
+  // Get the id for a given drum pad when the user selects it either by clicking on it or pressing its keyboard shortcut
+   function getAudioID(e) {
     let audioID = "";
     if(e.key == undefined) {
       audioID = e.target.innerText;
     } else {
       audioID = e.key.toUpperCase();
     }
+    return audioID;
+  }
 
-    // Use the object id to select the chosen drum pad object
-    let drumPad = drumPadDetails.find(pad => pad.id === audioID);
-   
-    // Update UI using properties from the drum pad object
+  // Update UI using properties from the drum pad object
+  function updateUI(drumPad) {
     $("#display")
       .removeClass("border-popsicle border-sunshine border-smurf border-peach border-violet-sky border-bubble-gum border-leaf border-lagoon border-sea")
       .addClass(`border-${drumPad.border}`)
