2657c2c220622f17e2e178d3e2ea793d04fcd9f7
Hannah Robinson <hannah.robinson.git@gmail.com>
2021-08-09 20:45:12 +1200

    Add error handling to drumBeatHandler for when users click an unassigned key.

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-import drumPadDetails from './modules/drumPadDetails.js'
+import drumPadDetails from './modules/drumPadDetails.js';
 
-$(function(){
- 
+$(function () {
   const drumBeatHandler = (e) => {
     const audioID = getAudioID(e);
     // Use the object id to select the chosen drum pad object
-    const drumPad = drumPadDetails.find(pad => pad.id === audioID);
-    updateUI(drumPad);
-  }
+    try {
+      const drumPad = drumPadDetails.find((pad) => pad.id === audioID);
+      updateUI(drumPad);
+    } catch (error) {
+      console.log(
+        'It seems you need a bigger drum machine :) \nChoose a letter that you can see on the drum machine.'
+      );
+      alert(
+        'It seems you need a bigger drum machine 😊 \nChoose a letter that you can see on the drum machine.'
+      );
+    }
+  };
 
   // Get the id for a given drum pad when the user selects it either by clicking on it or pressing its keyboard shortcut
-   function getAudioID(e) {
-    let audioID = "";
-    if(e.key == undefined) {
+  function getAudioID(e) {
+    let audioID = '';
+    if (e.key == undefined) {
       audioID = e.target.innerText;
     } else {
       audioID = e.key.toUpperCase();
@@ -22,16 +30,16 @@ $(function(){
 
   // Update UI using properties from the drum pad object
   function updateUI(drumPad) {
-    $("#display")
-      .removeClass("border-popsicle border-sunshine border-smurf border-peach border-violet-sky border-bubble-gum border-leaf border-lagoon border-sea")
+    $('#display')
+      .removeClass(
+        'border-popsicle border-sunshine border-smurf border-peach border-violet-sky border-bubble-gum border-leaf border-lagoon border-sea'
+      )
       .addClass(`border-${drumPad.border}`)
-      .text(drumPad.display)
-    $(`#${drumPad.id}`).trigger("play");
+      .text(drumPad.display);
+    $(`#${drumPad.id}`).trigger('play');
   }
 
   // Event listeners
-  $(".drum-pad").click(drumBeatHandler);
+  $('.drum-pad').click(drumBeatHandler);
   $(document).keypress(drumBeatHandler);
-}); 
-
-
+});
